2a62a71335b162ad2d8439a4c6eb6b9788bbdcb1
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-07-18 16:42:01 +0200

    Implement user interface and basic functions

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+const display = document.querySelector('.text');
+const buttons = document.querySelectorAll('.buttons button');
+// const numberButtons = document.querySelectorAll('.num-btn');
+// const OperatorButtons = document.querySelectorAll('.op-btn');
+// const clearButton = document.querySelector('.cl-btn');
+let displayValue = "";
+let leftValue = "0";
+let operator = "";
+let rightValue = "";
+let overWriteLeft = false;
+populateDisplay(displayValue);
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const input = e.target.textContent;
+        console.log(input);
+        if(!isNaN(+input)){
+            evaluateNumberInput(input);
+        }
+        else if(input === "C"){
+            clearDisplay(input);
+        }
+        else{
+            evaluateOperatorInput(input);
+        }
+        populateDisplay();
+    });
+});
+
+function evaluateNumberInput(digit){
+    if(!operator){
+        if(overWriteLeft){
+            overWriteLeft = false;
+            leftValue = "0";
+        }
+        leftValue += digit;
+    }
+    else{
+        rightValue += digit;
+    }
+}
+
+function evaluateOperatorInput(op){
+    
+    if(!rightValue){
+        operator = op === "=" ? operator : op;
+    }
+    else if(op === "="){
+        leftValue = operate(leftValue, rightValue, operator);
+        operator = "";
+        rightValue = "";
+        overWriteLeft = true;
+    }
+    else{
+        leftValue = operate(leftValue, rightValue, operator);
+        operator = op;
+        rightValue = "";
+    }
+}
+
+function clearDisplay(){
+    leftValue = "0";
+    operator = "";
+    rightValue = "";
+}
+
+function populateDisplay(){
+    if(!operator || operator === "="){
+        displayValue = `${+leftValue}`;
+    }
+    else if(!rightValue){
+        displayValue = `${+leftValue} ${operator}`;
+    }
+    else {
+        displayValue = `${+leftValue} ${operator} ${rightValue}`;
+    }
+    display.textContent = displayValue;
+}
+
 function add(a, b){
     return +a + +b;
 }

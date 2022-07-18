5611cbc4601adcc4400aba6345e4dea7961173d9
Nagy-Zsugya Martin Albert <zsugya.martin@gmail.com>
2022-07-18 13:36:44 +0200

    Add basic logic functions

---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b){
+    return +a + +b;
+}
+
+function subtract(a, b){
+    return +a - +b;
+}
+
+function multiply(a, b){
+    return +a * +b;
+}
+
+function divide(a, b){
+    return +a / +b;
+}
+
+function operate(a, b, op){
+    let result;
+    switch(op){
+        case "+":
+            result = add(a, b);
+            break;
+        case "-":
+            result = subtract(a, b);
+            break;
+        case "*":
+            result = multiply(a, b);
+            break;
+        case "/":
+            result = divide(a, b);
+            break;
+        default:
+            result = "invalid operator";
+    }
+    return result;
+}
+
